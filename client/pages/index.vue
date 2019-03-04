3e9b2f4ed73959905bc7b4b63e01c3e4ff5100e4
Sherin Binu <sherinbinu@hotmail.com>
2019-03-04 17:46:53 +0530

    set and get expenses

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,13 +1,9 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-expense-tracker
-</h1>
-      <h2 class="subtitle">
-a simple app to track your expense
-</h2>
+      <logo/>
+      <h1 class="title">expense-tracker</h1>
+      <h2 class="subtitle">a simple app to track your expense</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
 
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
+});
+
 app.get('/', (req, res) => {
-  res.send('Hi');
+  redisClient.hgetall('expenses', (error, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/', (req, res) => {
+  redisClient.incr('index', index => {
+    console.log(index);
+    console.log(typeof index);
+    const jsonBody = JSON.stringify({
+      message: req.body.message,
+      value: req.body.value
+    });
+    redisClient.hset('expenses', String(index), jsonBody, number => {
+      res.send(number);
+    });
+  });
 });
 
 app.listen(5000);
